65e67ee3f40f9529fda788132c0ee917f2c1da92
HalaSHream <151677768+HalaSHream@users.noreply.github.com>
2024-12-13 22:09:25 +0200

    Add files via upload
    customer Table controller

---
+++ src/main/java/com/example/realestate/controllers/customerTableController.java
@@ -0,0 +1,44 @@
+package com.example.realestate.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class customerTableController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button AddNewCustomer_btn;
+    @FXML
+    private Label CustomerTableL;
+
+    @FXML
+    private void initialize() {
+        AddNewCustomer_btn.setOnAction(event -> {
+            try {
+                navigateToAddCustomerDetails();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void navigateToAddCustomerDetails() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addCustomerDetails.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) AddNewCustomer_btn.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Add Customer Details");
+    }
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
