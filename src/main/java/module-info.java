cd0bd6c695113174f7584bd713fb62cb869976af
Dana-Morgan <dana.morgan196@gmail.com>
2024-12-13 16:47:06 +0200

    add logging module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.realestate {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+adda    requires java.logging;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
