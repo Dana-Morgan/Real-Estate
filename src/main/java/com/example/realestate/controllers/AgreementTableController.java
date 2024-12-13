f7dbf3dd6867b5554d9aa6e16cd7be2c1def4416
Dana-Morgan <dana.morgan196@gmail.com>
2024-12-14 01:53:03 +0200

    add the navbar for all main pages

--- src/main/java/com/example/realestate/controllers/AgreementTableController.java
+++ src/main/java/com/example/realestate/controllers/AgreementTableController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,14 +19,10 @@ public class AgreementTableController implements Initializable {
     private static final Logger LOGGER = Logger.getLogger(AgreementTableController.class.getName());
 
     @FXML
-    private Button backbtnAT;
-
-    @FXML
     private Button addAgreementbtn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        backbtnAT.setOnAction(event -> navigateTo("/com/example/realestate/views/HomePage.fxml", "Home Page"));
         addAgreementbtn.setOnAction(event -> navigateTo("/com/example/realestate/views/AgreementDetails.fxml", "Add Agreement"));
     }
 
@@ -35,16 +31,9 @@ public class AgreementTableController implements Initializable {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Parent root = loader.load();
 
-            Stage stage;
-            if ("Add Agreement".equals(title)) {
-                stage = new Stage();
-            } else {
-                stage = (Stage) backbtnAT.getScene().getWindow();
-            }
-
+            Stage stage = (Stage) addAgreementbtn.getScene().getWindow();
             stage.setScene(new Scene(root, 1280, 832));
             stage.setTitle(title);
-            stage.show();
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "Error: Unable to load " + fxmlPath, e);
         }
