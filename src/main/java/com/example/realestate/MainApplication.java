0a1ee60e7cf8e78dba76539e0ef81deba55c8d68
Dana-Morgan <dana.morgan196@gmail.com>
2024-12-13 23:01:12 +0200

    edit main app

--- src/main/java/com/example/realestate/MainApplication.java
+++ src/main/java/com/example/realestate/MainApplication.java
@@ -10,13 +10,12 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/example/realestate/views/CustomerInteractionDetails.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/example/realestate/views/HomePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 832);
-        stage.setTitle("Home");
+        stage.setTitle("Real Estate Application");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
