8ad12dd3a19d1fdd117af748b8663c30ed395abf
Dana-Morgan <dana.morgan196@gmail.com>
2024-12-20 15:46:26 +0200

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.realestate {
     requires javafx.fxml;
     requires javafx.web;
     requires java.logging;
-    opens com.example.realestate.models to javafx.base;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -17,14 +15,13 @@ module com.example.realestate {
     requires java.naming;
     requires java.sql;
 
-
+    opens com.example.realestate.models to javafx.base, org.hibernate.orm.core;
     opens com.example.realestate to javafx.fxml;
     opens com.example.realestate.utils to javafx.fxml;
-    opens com.example.realestate.models to org.hibernate.orm.core;
+    opens com.example.realestate.controllers to javafx.fxml;
 
     exports com.example.realestate;
     exports com.example.realestate.utils;
     exports com.example.realestate.models;
     exports com.example.realestate.controllers;
-    opens com.example.realestate.controllers to javafx.fxml;
 }
