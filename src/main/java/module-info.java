548d4dd1e67c93bc99fec2e56e48103255ffcbf7
Dana-Morgan <dana.morgan196@gmail.com>
2024-12-20 20:11:48 +0200

    database connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.example.realestate {
     requires javafx.fxml;
     requires javafx.web;
     requires java.logging;
-    opens com.example.realestate.models to javafx.base;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -12,9 +11,20 @@ module com.example.realestate {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires java.sql;
+    requires mysql.connector.java;
+
 
     opens com.example.realestate to javafx.fxml;
+    opens com.example.realestate.utils to javafx.fxml;
+    opens com.example.realestate.models to org.hibernate.orm.core;
+
     exports com.example.realestate;
+    exports com.example.realestate.utils;
+    exports com.example.realestate.models;
     exports com.example.realestate.controllers;
     opens com.example.realestate.controllers to javafx.fxml;
 }
