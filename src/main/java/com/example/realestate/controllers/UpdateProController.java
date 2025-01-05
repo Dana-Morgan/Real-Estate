e9531b97665358cff6f17c7e6b71e38c96e38119
Dana-Morgan <dana.morgan196@gmail.com>
2025-01-05 22:37:19 +0200

    .

--- src/main/java/com/example/realestate/controllers/UpdateProController.java
+++ src/main/java/com/example/realestate/controllers/UpdateProController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class UpdateCustomerDetailsController implements Initializable {
+public class UpdateProController implements Initializable {
 
     @FXML
     private TextField customerName;
